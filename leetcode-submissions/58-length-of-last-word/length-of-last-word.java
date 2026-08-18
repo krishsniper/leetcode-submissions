5497fadfea072458ce9eaa022a89538a7c89d1b0
Krishan R <krishan.rajesh2@gmail.com>
2026-08-18 15:34:33 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (96.93%) - LeetSync

---
+++ leetcode-submissions/58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,19 @@
+class Solution {
+    static int lengthOfLastWord(String s) {
+        int length = 0;
+        
+        
+        for (int i = s.length() - 1; i >= 0; i--) {
+            
+            if (s.charAt(i) != ' ') {
+                length++;
+            } 
+           
+            else if (length > 0) {
+                break;
+            }
+        }
+        
+        return length;
+    }
+}
