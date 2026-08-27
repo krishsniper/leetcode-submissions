8cfc5c3ed6a162e4d5b685b45c3121df64387683
Krishan R <krishan.rajesh2@gmail.com>
2026-08-27 18:11:21 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (12.22%) - LeetSync

---
+++ leetcode-submissions/83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.c
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode* deleteDuplicates(struct ListNode* head) {
+    if (head == NULL) {
+        return NULL;
+    }
+    
+    struct ListNode* current = head;
+    
+    while (current != NULL && current->next != NULL) {
+        if (current->val == current->next->val) {
+            struct ListNode* duplicate = current->next;
+            current->next = current->next->next;
+            
+            free(duplicate);
+        } else {
+            current = current->next;
+        }
+    }
+    
+    return head;
+}
